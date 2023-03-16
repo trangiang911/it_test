04128adab33fa27f24b6f62c33b34843311c2d31
giangtb <giangtb@rikkeisoft.com>
2023-03-16 09:26:01 +0700

    commit

--- ios/RunnerTests/RunnerTests.m
+++ ios/RunnerTests/RunnerTests.m
@@ -1,23 +1,4 @@
-#import <XCTest/XCTest.h>
-#import <Flutter/Flutter.h>
+@import XCTest;
+@import integration_test;
 
-@interface RunnerTests : XCTestCase
-
-@end
-
-@implementation RunnerTests
-
-- (void)setUp {
-  self.continueAfterFailure = NO;
-  [[[XCUIApplication alloc] init] launch];
-}
-
-- (void)testExample {
-  FlutterDriver *driver = [[FlutterDriver alloc] initWithHost:@"localhost" port:12345];
-  [driver waitUntilReady];
-  FlutterDriverResult *result = [driver checkHealth];
-  [driver close];
-  XCTAssertEqual(FlutterDriverSuccess, result.status);
-}
-
-@end
+INTEGRATION_TEST_IOS_RUNNER(RunnerTests)
