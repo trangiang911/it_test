1468da50cff07bbadb15beb2b35dda3b316ab24e
giangtb <giangtb@rikkeisoft.com>
2023-03-16 08:23:52 +0700

    commit

---
+++ ios/Runner/Tests/RunnerTests.m
@@ -0,0 +1,15 @@
+#import <XCTest/XCTest.h>
+#import <Flutter/Flutter.h>
+
+@interface RunnerTests : XCTestCase
+@end
+
+@implementation RunnerTests
+
+- (void)testRunner {
+  // This is a placeholder test case that does nothing.
+  // You should replace this with your own test case(s).
+  XCTAssert(YES);
+}
+
+@end
