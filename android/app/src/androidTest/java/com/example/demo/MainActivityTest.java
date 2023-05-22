d1be61a1f13671bc2bed6a622a2ff149da2c209f
giangtb <giangtb@rikkeisoft.com>
2023-05-22 17:44:02 +0700

    commit

--- android/app/src/androidTest/java/com/example/demo/MainActivityTest.java
+++ android/app/src/androidTest/java/com/example/demo/MainActivityTest.java
@@ -10,8 +10,13 @@ import org.junit.runner.RunWith;
 // Import MainActivity from YOUR package
 import com.example.demo.MainActivity;
 
-@RunWith(FlutterTestRunner.class)
+import pl.leancode.patrol.PatrolTestRule;
+import pl.leancode.patrol.PatrolTestRunner;
+
+@RunWith(PatrolTestRunner.class)
 public class MainActivityTest {
+    //    @Rule
+//    public ActivityTestRule<MainActivity> rule = new ActivityTestRule<>(MainActivity.class, true, false);
     @Rule
-    public ActivityTestRule<MainActivity> rule = new ActivityTestRule<>(MainActivity.class, true, false);
+    public PatrolTestRule<MainActivity> rule = new PatrolTestRule<>(MainActivity.class);
 }
--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -1,24 +1,45 @@
+import 'package:demo/main.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:integration_test/integration_test.dart';
 import 'package:demo/main.dart' as app;
 
+import 'package:patrol/patrol.dart';
+
 void main() {
-  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+  patrolTest(
+    'records sounds and saves it to internal storage',
+    nativeAutomation: true,
+    ($) async {
+      app.main();
+      await $.pumpAndSettle();
+      await $.native.tap(Selector(text: 'Allow access to manage all files'));
+      await $.native.pressBack();
+      await $.native.grantPermissionWhenInUse();
+      final fabFinder = find.byType(FloatingActionButton);
+      await $.tap(fabFinder);
+      await $.tap(fabFinder);
+      await $.pumpAndSettle();
+      final counterFinder = find.text('2');
+      expect(counterFinder, findsOneWidget);
+    },
+  );
 
-  testWidgets('Increment counter', (WidgetTester tester) async {
-    app.main(); // khởi tạo ứng dụng
-    await tester.pumpAndSettle(); // chờ cho ứng dụng hiển thị
-
-    // tìm floating action button và nhấn vào nó
-    final fabFinder = find.byType(FloatingActionButton);
-    expect(fabFinder, findsOneWidget);
-    await tester.tap(fabFinder);
-    await tester.tap(fabFinder);
-    await tester.pumpAndSettle(const Duration(seconds: 10));
-
-    // kiểm tra xem giá trị của _counter có tăng lên không
-    final counterFinder = find.text('2');
-    expect(counterFinder, findsOneWidget);
-  });
+  //
+  // IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+  //
+  // testWidgets('Increment counter', (WidgetTester tester) async {
+  //   app.main(); // khởi tạo ứng dụng
+  //   await tester.pumpAndSettle(); // chờ cho ứng dụng hiển thị
+  //
+  //   // tìm floating action button và nhấn vào nó
+  //   final fabFinder = find.byType(FloatingActionButton);
+  //   expect(fabFinder, findsOneWidget);
+  //   await tester.tap(fabFinder);
+  //   await tester.tap(fabFinder);
+  //   await tester.pumpAndSettle(const Duration(seconds: 10));
+  //
+  //   // kiểm tra xem giá trị của _counter có tăng lên không
+  //   final counterFinder = find.text('2');
+  //   expect(counterFinder, findsOneWidget);
+  // });
 }
